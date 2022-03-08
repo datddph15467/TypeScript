357021aefb68641a3289120a9de8059fd433e72f
Dinh Dat <datddph15467@fpt.edu.vn>
2022-03-08 13:12:59 +0700

    update

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+export{};
+const a :number = 20;
+const b :number = 30;
+const c :number = 30;
+const name :string = "Du Dinh Dat";
+const status :boolean = true;
+const info :object = {};
+const arr :string[] = ['a','b','c'];
+const Numberarr :number[] = [1,2,3];
+const objectArr :object[] = [ { id: 1 ,name:'Dinh Dat'},{ id : 2 , name:'Dao Thi' }]
+
+function sum(numA:number,numB:number):number{
+    return numA+numB;
+
+}
+sum(a,b);
+console.log(objectArr);
